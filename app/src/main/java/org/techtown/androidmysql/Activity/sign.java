b23279d918301697be7dda62fb02efb271dd8efd
power90 <ssotr12@gmail.com>
2020-02-19 15:10:13 +0900

    테스트3

--- app/src/main/java/org/techtown/androidmysql/Activity/sign.java
+++ app/src/main/java/org/techtown/androidmysql/Activity/sign.java
@@ -22,7 +22,7 @@ import java.net.URL;
 
 public class sign extends AppCompatActivity {
 
-    //sdkfslakfjasldf
+    //커밋 테스트 주석
     //접속정보와 태그명 지정
     private static String IP_ADDRESS = "15.164.218.247";
     private static String TAG = "signtest";
