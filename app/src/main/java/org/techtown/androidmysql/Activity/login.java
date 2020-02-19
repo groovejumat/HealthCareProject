a0069374dd817d8ae2e2d04dec7e07740ea07514
intae-98 <askdfjaslk@naver.com>
2020-02-19 15:25:12 +0900

    커밋 확인

--- app/src/main/java/org/techtown/androidmysql/Activity/login.java
+++ app/src/main/java/org/techtown/androidmysql/Activity/login.java
@@ -24,11 +24,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.MessageDigest;
 
-
 public class login extends AppCompatActivity {
-
-
-    // 안스 재부팅하고 나서 커밋222222222
 
     //접속정보와 태그명 지정
     private static String IP_ADDRESS = "15.164.218.247";
